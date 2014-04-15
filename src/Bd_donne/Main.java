a09c318bff6830066969bc8b2955fa8b82ff282d
Didia Aristote Diasonama <didia@eventbuck.com>
2014-04-15 16:44:53 -0400

    Modification processus de connexion
    Ajout de la fonction make connexion qui permet de se connecter a la base
    de données

--- src/Bd_donne/Main.java
+++ src/Bd_donne/Main.java
@@ -11,26 +11,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			int i = 0;
-
-			do {
 				Connexion m_connexion = new Connexion();
-				if (m_connexion.getAcceOK() == true)
-					break;
-				else
-					i++;
-			} while (i < 3);
-
-			if (i == 3)
-
-			{
-				JOptionPane
-						.showMessageDialog(null,
-								"Vous avez fait 3 tentatives de connexion erronées... :( bye");
-			} else {
-				Interface mon_Interface = new Interface();
-				mon_Interface.setVisible(true);
-			}
+				if(m_connexion.makeConnexion())
+				{
+					Interface mon_Interface = new Interface();
+					mon_Interface.setVisible(true);
+				}
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
